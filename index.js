813f5cbbdd86767ff1020d6b29390fc1ee822e11
Spencer Griffin <spencekarl@gmail.com>
2016-05-12 18:17:55 -0400

    added comments and a bit of refactoring

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ function EventEmitter() {
 
 EventEmitter.prototype.on = function (name, handler) {
   if (typeof name !== 'string' || typeof handler !== 'function') throw new TypeError;
+  // if we get past the error handling we are ready to bind the handler functions
 
+  // the first instance of the event
   if (!this.events[name]) {
     this.events[name] = handler;
   }
+  // create an array of handlers if this is the second instance of the event
   else if (this.listeners == 1) {
     this.events[name] = [this.events[name], handler];
   }
+  // push all new handlers to the previously created array
   else {
     this.events[name].push(handler);
   }
@@ -21,29 +25,32 @@ EventEmitter.prototype.on = function (name, handler) {
 }
 
 EventEmitter.prototype.off = function () {
+  var name = arguments[0];
+  var handler = arguments[1];
   var length = arguments.length;
 
-  if (length > 0 && typeof arguments[0] !== 'string') throw new TypeError;
-  if (length > 1 && (typeof arguments[1] === undefined || typeof arguments[1] !== 'function')) throw new TypeError;
+  if (length > 0 && typeof name !== 'string') throw new TypeError;
+  if (length > 1 && (typeof handler === undefined || typeof handler !== 'function')) throw new TypeError;
 
-  if (!length) {
+  if (length == 0) {  // reset if no parameters passed in
     this.events = {};
     this.listeners = 0;
+    return this;
   }
 
-  if (this.events[arguments[0]]) {
-    if (arguments[1]) {
-      length = this.events[arguments[0]].length;
+  if (this.events[name]) {
+    if (handler) {
+      length = this.events[name].length;
       for (i = length - 1; i >= 0; i--) {
-        if (this.events[arguments[0]][i] == arguments[1]) {
+        if (this.events[name][i] == handler) {
           this.listeners--;
-          this.events[arguments[0]].splice(i, 1);
+          this.events[name].splice(i, 1);
         }
       }
     }
     else {
-      this.listeners -= this.events[arguments[0]].length;
-      delete this.events[arguments[0]];
+      this.listeners -= this.events[name].length;
+      delete this.events[name];
     }
   }
 
@@ -54,24 +61,28 @@ EventEmitter.prototype.emit = function () {
   if (typeof arguments[0] !== 'string') throw new TypeError;
   if (typeof arguments[1] === 'function') return this;
 
-  var handler = this.events[arguments[0]];
+  var handler = this.events[arguments[0]]; // get handler(s) bound to event
   var length;
-  var args = [];
 
+  // handler could be a function
   if (typeof handler === 'function') {
+    var args = [];
     length = arguments.length;
 
+    // build a list of arguments to pass to handler
     for (i = 0; i < length; i++) {
       args[i] = arguments[i + 1];
     }
     handler.apply(this, args);
   }
+  // ...or handler could be many functions
   else if (typeof handler === 'object'){
-    var handlers = handler.slice("");
-    length = handlers.length;
+    var list = handler.slice(""); // make an array of functions
+    length = list.length;
 
+    // call each function
     for (i = 0; i < length; i++) {
-      handlers[i].call(this);
+      list[i].call(this);
     }
   }
 }
