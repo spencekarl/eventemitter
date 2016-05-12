1fdc2e546ff4af5079ae7a5073034d42718cc0a3
Spencer Griffin <spencekarl@gmail.com>
2016-05-11 21:20:41 -0400

    added .emit and fixed test-case issues with .on

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 function EventEmitter() {
   this.listeners = 0;
+  this.events = {};
 }
 
-EventEmitter.prototype.on = function (arg1, arg2) {
-  if (typeof arg1 !== 'string' || typeof arg2 !== 'function') throw new TypeError;
+EventEmitter.prototype.on = function (event, handler) {
+  if (typeof event !== 'string' || typeof handler !== 'function') throw new TypeError;
+
+  if (!this.events[event]) {
+    this.events[event] = handler;
+  }
+  else if (this.events[event] === typeof Array) {
+    this.events[event].push(handler);
+  }
+  else {
+    this.events[event] = [this.events[event], handler];
+  }
 
   this.listeners++;
   return this;
 }
 
-EventEmitter.prototype.emit = function (arg1) {
-  if (typeof arg1 !== 'string') throw new TypeError;
+EventEmitter.prototype.off = function () {
 
-  return this;
+}
+
+EventEmitter.prototype.emit = function () {
+  if (typeof arguments[0] !== 'string') throw new TypeError;
+  if (typeof arguments[1] === 'function') return this;
+
+  var handler = this.events[arguments[0]];
+  var args = [];
+  var length;
+
+  if (typeof handler === 'function') {
+    length = arguments.length;
+
+    for (i = 0; i < length; i++) {
+      args[i] = arguments[i + 1];
+    }
+    handler.apply(this, args);
+  }
+  else {
+    var handlers = handler.slice("");
+    length = handlers.length;
+
+    for (i = 0; i < length; i++) {
+      handlers[i].call(this);
+    }
+  }
 }
 
 //need to understand .exports better
