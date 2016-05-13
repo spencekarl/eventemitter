81c344c0a834560f1a3c07b76ff94908c7895f67
Spencer Griffin <spencekarl@gmail.com>
2016-05-13 08:38:39 -0400

    uncommented rest of tests

--- test/core.js
+++ test/core.js
@@ -234,188 +234,188 @@ describe( 'EventEmitter (core)', function() {
     });
 
   });
-  //
-  // describe( '#off', function() {
-  //
-  //   it( 'should be a method', function() {
-  //     var EventEmitter = require('../index');
-  //
-  //     assert.equal( typeof EventEmitter.prototype.off, 'function', 'EventEmitter#off is not a function' );
-  //   });
-  //
-  //   it( 'should remove all listeners when no args are passed', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //     var called = 0;
-  //     var result;
-  //
-  //     emitter.on( 'a', function() {
-  //       called++;
-  //     });
-  //
-  //     emitter.on( 'b', function() {
-  //       called++;
-  //     });
-  //
-  //     result = emitter.off();
-  //
-  //     emitter.emit('a');
-  //     emitter.emit('b');
-  //
-  //     assert.strictEqual( called, 0, 'One or more events was not unbound' );
-  //     assert.strictEqual( result, emitter, 'Did not return `this`' );
-  //     assert.strictEqual( emitter.listeners, 0, '`listeners` value was not correctly updated' );
-  //   });
-  //
-  //   it( 'should throw a TypeError when first arg is not a string', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //
-  //     assert.throws(function() {
-  //       emitter.off( 123 );
-  //     }, TypeError );
-  //   });
-  //
-  //   it( 'should throw a TypeError when first arg is undefined', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //
-  //     assert.throws(function() {
-  //       emitter.off( undefined );
-  //     }, TypeError );
-  //   });
-  //
-  //   it( 'should not throw when no matching events found', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //
-  //     assert.doesNotThrow(function() {
-  //       emitter.off( 'foo' );
-  //     }, 'with one arg' );
-  //
-  //     assert.doesNotThrow(function() {
-  //       emitter.off( 'foo', function(){});
-  //     }, 'with two args');
-  //   });
-  //
-  //   it( 'should not throw when no matching functions found', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //
-  //     emitter.on('foo', function(){});
-  //
-  //     assert.doesNotThrow(function() {
-  //       emitter.off( 'foo', function(){});
-  //     });
-  //   });
-  //
-  //   it( 'should remove bound listeners by event name', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //     var calledA = 0;
-  //     var calledB = 0;
-  //     var result;
-  //
-  //     emitter.on( 'a', function() {
-  //       calledA++;
-  //     });
-  //
-  //     emitter.on( 'a', function() {
-  //       calledA++;
-  //     });
-  //
-  //     emitter.on( 'b', function() {
-  //       calledB++;
-  //     });
-  //
-  //     result = emitter.off('a');
-  //
-  //     emitter.emit('a');
-  //     emitter.emit('b');
-  //
-  //     assert.strictEqual( calledA, 0, 'One or more events was not unbound' );
-  //     assert.strictEqual( calledB, 1, 'A non-targeted listener was unbound' );
-  //     assert.strictEqual( emitter.listeners, 1, '`listeners` value was not correctely updated' );
-  //     assert.strictEqual( result, emitter, 'Did not return `this`' );
-  //   });
-  //
-  //   it( 'should throw a TypeError when second arg is not a function', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //
-  //     assert.throws(function() {
-  //       emitter.off( 'foo', [] );
-  //     }, TypeError );
-  //   });
-  //
-  //   it( 'should throw a TypeError when second arg is undefined', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //
-  //     assert.throws(function() {
-  //       emitter.off( 'foo', undefined );
-  //     }, TypeError );
-  //   });
-  //
-  //   it( 'should remove bound listeners by event name and callback', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter = new EventEmitter();
-  //     var calledA = 0;
-  //     var calledB = 0;
-  //     var result;
-  //
-  //     function fnA() {
-  //       calledA++;
-  //     }
-  //
-  //     function fnB() {
-  //       calledB++;
-  //     }
-  //
-  //     emitter.on( 'a', fnA );
-  //     emitter.on( 'a', fnA );
-  //     emitter.on( 'a', fnB );
-  //
-  //     // only targeting 'a' events, so this should not be unbound
-  //     // when we call `off( 'a', fnA )`
-  //     emitter.on( 'b', fnA );
-  //
-  //     result = emitter.off( 'a', fnA );
-  //
-  //     emitter.emit('a');
-  //
-  //     assert.strictEqual( calledA, 0, 'One or more events was not unbound' );
-  //     assert.strictEqual( calledB, 1, 'A non-targeted listener was unbound' );
-  //     assert.strictEqual( emitter.listeners, 2, '`listeners` value was not correctely updated' );
-  //     assert.strictEqual( result, emitter, 'Did not return `this`' );
-  //
-  //     // make sure our 'b' event was not unbound
-  //     emitter.emit('b');
-  //     assert.strictEqual( calledA, 1, 'A non-targeted listener was unbound' );
-  //   });
-  //
-  //   it( 'should scope events to the instance they were bound to', function() {
-  //     var EventEmitter = require('../index');
-  //     var emitter1 = new EventEmitter();
-  //     var emitter2 = new EventEmitter();
-  //     var called = 0;
-  //
-  //     emitter1.on( 'foo', function() {
-  //       called++;
-  //     });
-  //
-  //     // should not be unbound
-  //     emitter2.on( 'foo', function() {
-  //       called++;
-  //     });
-  //
-  //     emitter1.off('foo');
-  //
-  //     emitter2.emit('foo');
-  //
-  //     assert.strictEqual( called, 1, 'Event unbound from non-targeted instance' );
-  //   });
-  //
-  // });
+
+  describe( '#off', function() {
+
+    it( 'should be a method', function() {
+      var EventEmitter = require('../index');
+
+      assert.equal( typeof EventEmitter.prototype.off, 'function', 'EventEmitter#off is not a function' );
+    });
+
+    it( 'should remove all listeners when no args are passed', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+      var called = 0;
+      var result;
+
+      emitter.on( 'a', function() {
+        called++;
+      });
+
+      emitter.on( 'b', function() {
+        called++;
+      });
+
+      result = emitter.off();
+
+      emitter.emit('a');
+      emitter.emit('b');
+
+      assert.strictEqual( called, 0, 'One or more events was not unbound' );
+      assert.strictEqual( result, emitter, 'Did not return `this`' );
+      assert.strictEqual( emitter.listeners, 0, '`listeners` value was not correctly updated' );
+    });
+
+    it( 'should throw a TypeError when first arg is not a string', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+
+      assert.throws(function() {
+        emitter.off( 123 );
+      }, TypeError );
+    });
+
+    it( 'should throw a TypeError when first arg is undefined', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+
+      assert.throws(function() {
+        emitter.off( undefined );
+      }, TypeError );
+    });
+
+    it( 'should not throw when no matching events found', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+
+      assert.doesNotThrow(function() {
+        emitter.off( 'foo' );
+      }, 'with one arg' );
+
+      assert.doesNotThrow(function() {
+        emitter.off( 'foo', function(){});
+      }, 'with two args');
+    });
+
+    it( 'should not throw when no matching functions found', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+
+      emitter.on('foo', function(){});
+
+      assert.doesNotThrow(function() {
+        emitter.off( 'foo', function(){});
+      });
+    });
+
+    it( 'should remove bound listeners by event name', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+      var calledA = 0;
+      var calledB = 0;
+      var result;
+
+      emitter.on( 'a', function() {
+        calledA++;
+      });
+
+      emitter.on( 'a', function() {
+        calledA++;
+      });
+
+      emitter.on( 'b', function() {
+        calledB++;
+      });
+
+      result = emitter.off('a');
+
+      emitter.emit('a');
+      emitter.emit('b');
+
+      assert.strictEqual( calledA, 0, 'One or more events was not unbound' );
+      assert.strictEqual( calledB, 1, 'A non-targeted listener was unbound' );
+      assert.strictEqual( emitter.listeners, 1, '`listeners` value was not correctely updated' );
+      assert.strictEqual( result, emitter, 'Did not return `this`' );
+    });
+
+    it( 'should throw a TypeError when second arg is not a function', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+
+      assert.throws(function() {
+        emitter.off( 'foo', [] );
+      }, TypeError );
+    });
+
+    it( 'should throw a TypeError when second arg is undefined', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+
+      assert.throws(function() {
+        emitter.off( 'foo', undefined );
+      }, TypeError );
+    });
+
+    it( 'should remove bound listeners by event name and callback', function() {
+      var EventEmitter = require('../index');
+      var emitter = new EventEmitter();
+      var calledA = 0;
+      var calledB = 0;
+      var result;
+
+      function fnA() {
+        calledA++;
+      }
+
+      function fnB() {
+        calledB++;
+      }
+
+      emitter.on( 'a', fnA );
+      emitter.on( 'a', fnA );
+      emitter.on( 'a', fnB );
+
+      // only targeting 'a' events, so this should not be unbound
+      // when we call `off( 'a', fnA )`
+      emitter.on( 'b', fnA );
+
+      result = emitter.off( 'a', fnA );
+
+      emitter.emit('a');
+
+      assert.strictEqual( calledA, 0, 'One or more events was not unbound' );
+      assert.strictEqual( calledB, 1, 'A non-targeted listener was unbound' );
+      assert.strictEqual( emitter.listeners, 2, '`listeners` value was not correctely updated' );
+      assert.strictEqual( result, emitter, 'Did not return `this`' );
+
+      // make sure our 'b' event was not unbound
+      emitter.emit('b');
+      assert.strictEqual( calledA, 1, 'A non-targeted listener was unbound' );
+    });
+
+    it( 'should scope events to the instance they were bound to', function() {
+      var EventEmitter = require('../index');
+      var emitter1 = new EventEmitter();
+      var emitter2 = new EventEmitter();
+      var called = 0;
+
+      emitter1.on( 'foo', function() {
+        called++;
+      });
+
+      // should not be unbound
+      emitter2.on( 'foo', function() {
+        called++;
+      });
+
+      emitter1.off('foo');
+
+      emitter2.emit('foo');
+
+      assert.strictEqual( called, 1, 'Event unbound from non-targeted instance' );
+    });
+
+  });
 
 });
